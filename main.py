1a96a62908e9e96495ad32b21885ed7c92af2a63
bdiverbridgewater <144378894+bdiverbridgewater@users.noreply.github.com>
2024-02-05 11:40:06 -0500

    Wrote some functions to set up the database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 from serpapi.google_search import GoogleSearch
 import secrets
+import sqlite3
+from typing import Tuple
 
 
 def job_search(result_offset) -> dict:
@@ -24,7 +26,33 @@ def write_dict_to_file(dict_input: dict, write_file):
     write_file.write(dict_as_string)
 
 
-if __name__ == "__main__":
+def open_database(file_name: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    database_connection = sqlite3.connect(file_name)
+    cursor = database_connection.cursor()
+    return database_connection, cursor
+
+
+def setup_database(cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs(
+        search_result INTEGER PRIMARY KEY,
+        job_title TEXT NOT NULL,
+        company_name TEXT NOT NULL,
+        location TEXT NOT NULL,
+        is_remote BOOLEAN,
+        description TEXT,
+        date_posted TEXT,
+        salary TEXT,
+        related_link TEXT)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS qualifications(
+    FOREIGN KEY (search_result) REFERENCES jobs(search_result))''')
+
+
+def close_database(connection: sqlite3.Connection):
+    connection.commit()
+    connection.close()
+
+
+def main():
     total_pages = 5
     results_per_page = 10
     current_pages = 0
@@ -38,3 +66,7 @@ if __name__ == "__main__":
         current_pages += 1
     search_results_file.close()
     print(f'Search Complete. Results saved to "{search_results_file.name}"')
+
+
+if __name__ == "__main__":
+    main()
