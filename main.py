9bc035773ee997fd8fd04bd0ad9b06a9366c2260
bdiverbridgewater <144378894+bdiverbridgewater@users.noreply.github.com>
2024-02-07 18:16:13 -0500

    changing import statements again

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import serpapi.google_search
+from serpapi import google_search
 import secrets
 import sqlite3
 from typing import Tuple
@@ -15,7 +15,7 @@ def job_search(result_offset) -> dict:
         "location": "Boston, Massachusetts, United States",
         "start": result_offset
     }
-    search = serpapi.google_search.GoogleSearch(params)
+    search = google_search.GoogleSearch(params)
     results = search.get_dict()
     return results
 
