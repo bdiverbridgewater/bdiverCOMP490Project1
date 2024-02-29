cff00c24ef458c8545d4300f6ed158038a9e9e1f
bdiverbridgewater <144378894+bdiverbridgewater@users.noreply.github.com>
2024-02-29 11:28:33 -0500

    adjusted test functions to fit the new format of the salary data

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,6 @@
 from openpyxl import load_workbook
-from main import (open_database, setup_database, close_database, job_search, try_to_get_salary, get_jobs_data,
-                  insert_jobs_to_database, get_excel_jobs)
+from main import (open_database, setup_database, close_database, job_search, get_jobs_data,
+                  insert_jobs_to_database, get_excel_jobs, get_salary)
 
 
 def test_database_functionality():
@@ -21,7 +21,14 @@ def test_database_functionality():
     assert job[5] == test_search[0].get("related_links")[0].get("link")
     assert job[6] == test_search[0]["detected_extensions"].get("work_from_home")
     assert job[7] == test_search[0]["detected_extensions"].get("posted_at")
-    assert job[8] == try_to_get_salary(test_search[0])
+    try:
+        benefits_section = test_search[0]["job_highlights"][2]
+    except IndexError:
+        benefits_section = None
+
+    min_salary, max_salary = get_salary(benefits_section, test_search[0]["description"])
+    assert job[8] == min_salary
+    assert job[9] == max_salary
     cursor.execute('''DROP TABLE jobs;''')
     cursor.execute('''DROP TABLE qualifications;''')
     close_database(connection)
